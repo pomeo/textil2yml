31f8116082071f0a0aac48e48a9e52bf7950f8a7
Sergey Ovechkin <me@sovechkin.com>
2013-10-23 23:26:54 +0400

    распаковываем архив

--- script.rb
+++ script.rb
@@ -3,6 +3,7 @@
 require 'rubygems'
 require 'mechanize'
 require 'open-uri'
+require 'zip'
 
 a = Mechanize.new { |agent|
   agent.user_agent_alias = 'Mac Safari'
@@ -19,3 +20,10 @@ end
 # используя куку качаем файл
 a.pluggable_parser.default = Mechanize::Download
 a.get('http://www.textilgroup.ru/xml/postelinoe_belie.zip').save!('postelinoe_belie.zip')
+
+# распаковываем архив
+Zip::File.open('postelinoe_belie.zip') do |zipfile|
+  zipfile.each do |file|
+    file.extract('output.xml')
+  end
+end
