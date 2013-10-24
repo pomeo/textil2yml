27505c0679b8323f42947c9a9d1f92dc6d462bcc
Sergey Ovechkin <me@sovechkin.com>
2013-10-24 18:20:16 +0400

    парсим скаченный xml

--- script.rb
+++ script.rb
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 require 'rubygems'
 require 'mechanize'
+require 'nokogiri'
 require 'open-uri'
 require 'zip'
 
@@ -26,4 +27,24 @@ Zip::File.open('postelinoe_belie.zip') do |zipfile|
   zipfile.each do |file|
     file.extract('output.xml')
   end
+end
+
+# парсим скаченный xml
+doc = Nokogiri.XML(File.open('output.xml', 'rb'))
+
+@xml = doc.xpath('//shop/products/product').map do |i|
+  {
+    'cat' => i.at_xpath('category').content,
+    'art' => i.at_xpath('articul').content,
+    'price' => i.at_xpath('price').content,
+    'desc' => i.at_xpath('description').content,
+    'img' => i.at_xpath('image_big').content,
+    'size' => i.at_xpath('size').content,
+    'mat' => i.at_xpath('material').content,
+    'brend' => i.at_xpath('brend').content,
+    'n_price' => i.at_xpath('name_price').content,
+    'n_nak' => i.at_xpath('name_nak').content,
+    'country' => i.at_xpath('country').content,
+    'a' => i.at_xpath('avail').content
+  }
 end
